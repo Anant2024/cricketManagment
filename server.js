f97a5790abcfb90893cb7348d4403aef38fc46a8
Anant Bhardwaj <anantbhardwaj8115@gmail.com>
2024-11-01 17:53:24 +0530

    port modified

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config()
 connectDB()
 .then(() => {
     app.listen(process.env.PORT|| 8000, () => {
-        console.log(`⚙️ Server is running at port :`);
+        console.log(`⚙️ Server is running at port :${process.env.PORT||8000}`);
     })
 })
 .catch((err) => {
